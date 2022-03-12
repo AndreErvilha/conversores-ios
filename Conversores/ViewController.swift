6280034697543366924fb4bd316c7acdf39d01c3
andreervilha <andre.ervilha@cwi.com.br>
2022-03-11 23:36:22 -0300

    start project launchPage mainPage

---
+++ Conversores/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Andre Eduardo Ribeiro Ervilha on 11/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnitOne: UIButton!
+    @IBOutlet weak var btUnitTwo: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showNext(_ sender: Any) {
+    }
+    
+    @IBAction func convert(_ sender: Any) {
+    }
+}
+
